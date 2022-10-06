3d1c94f1f0fcd50022cb11ac19f84b419e42b8e1
John Buchanan <john@buchanandevops.com>
2022-10-05 19:29:21 -0700

    more updates / testing on apple and android for responsive

--- content/full-stack/index.tsx
+++ content/full-stack/index.tsx
@@ -41,5 +41,5 @@ export default {
           description:
             'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
         },
-      ];
+      ]
 }
