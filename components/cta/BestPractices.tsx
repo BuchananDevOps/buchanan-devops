08d6df13bcc6bc9a6705e6f368f117359233c9eb
John Buchanan <john@buchanandevops.com>
2024-03-12 13:19:32 -0400

    changes to best websites cta

--- components/cta/BestPractices.tsx
+++ components/cta/BestPractices.tsx
@@ -7,19 +7,14 @@ const BestPractices: FC = () => {
   return (
     <Section id="best-practices" isBordered={false}>
       <h2 className="text-slate-900 text-center text-4xl tracking-tight font-extrabold sm:text-5xl dark:text-white">
-        “Best websites” don&apos;t actually work.
+        Is your website generating revenue?
       </h2>
       <figure>
         <blockquote>
           <p className="mt-6 max-w-3xl mx-auto text-lg text-center">
             True project success goes beyond the end result; it&apos;s about the
-            collaborative journey marked by innovation and resilience, turning
-            challenges into triumphs. While many claim to offer the{" "}
-            <span className="text-sky-500 font-semibold dark:text-sky-400">
-              &apos;best web design or SEO,&apos;
-            </span>{" "}
-            we understand it&apos;s more than just a popular keyword—it&apos;s
-            about genuine expertise and meaningful results.
+            generating revenue by engaging customers, turning
+            clicks into conversions. 
           </p>
         </blockquote>
         <figcaption className="mt-6 flex items-center justify-center space-x-4 text-left">
